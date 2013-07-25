49914c3e420bb12c249796e4ec9c46b30efcd42e
Martin Peeks <martinp23@googlemail.com>
2013-07-25 17:53:22 +0100

    update database install sql for revisions

--- install/elabftw.sql
+++ install/elabftw.sql
@@ -28,15 +28,23 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `experiments` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
   `date` int(10) unsigned NOT NULL,
-  `body` text NOT NULL,
   `status` varchar(255) NOT NULL,
   `links` varchar(255) DEFAULT NULL,
-  `userid` int(10) unsigned NOT NULL,
+  `userid_creator` int(10) unsigned NOT NULL,
+  `userid_closer` int(10) unsigned DEFAULT NULL,
+  `userid_witness` int(10) unsigned DEFAULT NULL,
   `elabid` varchar(255) NOT NULL,
   `locked` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
+  `closed` tinyint(1) NOT NULL DEFAULT '0',
+  `witnessed` tinyint(1) NOT NULL DEFAULT '0',
+  `rev_id` int(10) unsigned DEFAULT NULL,
+  `deleted` tinyint(1) unsigned DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `userid_idx` (`userid_creator`,`userid_witness`,`userid_closer`),
+  KEY `userid_witness_idx` (`userid_witness`),
+  KEY `userid_closer_idx` (`userid_closer`),
+  KEY `rev_id` (`rev_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -50,7 +58,7 @@ CREATE TABLE IF NOT EXISTS `experiments_links` (
   `item_id` int(10) unsigned NOT NULL,
   `link_id` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -63,8 +71,9 @@ CREATE TABLE IF NOT EXISTS `experiments_tags` (
   `tag` varchar(255) NOT NULL,
   `item_id` int(10) unsigned NOT NULL,
   `userid` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  KEY `userid_idx` (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -77,8 +86,9 @@ CREATE TABLE IF NOT EXISTS `experiments_templates` (
   `body` text,
   `name` varchar(255) NOT NULL,
   `userid` int(10) unsigned DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  KEY `userid_idx` (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -94,8 +104,10 @@ CREATE TABLE IF NOT EXISTS `items` (
   `rating` tinyint(10) DEFAULT '0',
   `type` int(10) unsigned NOT NULL,
   `userid` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  KEY `userid_idx` (`userid`),
+  KEY `type_idx` (`type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -107,8 +119,9 @@ CREATE TABLE IF NOT EXISTS `items_tags` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `tag` varchar(255) NOT NULL,
   `item_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  KEY `item_id_idx` (`item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -128,6 +141,31 @@ CREATE TABLE IF NOT EXISTS `items_types` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `revisions`
+--
+
+CREATE TABLE IF NOT EXISTS `revisions` (
+  `rev_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `experiment_id` int(10) unsigned DEFAULT NULL,
+  `item_id` int(10) unsigned DEFAULT NULL,
+  `rev_notes` varchar(140) DEFAULT NULL,
+  `rev_parent_id` int(10) unsigned DEFAULT NULL,
+  `rev_body` text DEFAULT NULL,
+  `rev_title` varchar(255) DEFAULT NULL,
+  `rev_structure` varchar(45) DEFAULT NULL,
+  `rev_links` varchar(255) DEFAULT NULL,
+  `rev_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`rev_id`),
+  UNIQUE KEY `revid_UNIQUE` (`rev_id`),
+  KEY `user_id_idx` (`user_id`),
+  KEY `experiment_id_idx` (`experiment_id`),
+  KEY `item_id_idx` (`item_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `uploads`
 --
 
@@ -137,11 +175,12 @@ CREATE TABLE IF NOT EXISTS `uploads` (
   `long_name` text NOT NULL,
   `comment` text NOT NULL,
   `item_id` int(10) unsigned DEFAULT NULL,
-  `userid` text NOT NULL,
+  `userid` int(10) unsigned NOT NULL,
   `type` varchar(255) NOT NULL,
   `date` date DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  KEY `userid_idx` (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -181,6 +220,40 @@ CREATE TABLE IF NOT EXISTS `users` (
   `validated` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`userid`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+
+ALTER TABLE `experiments`
+  ADD CONSTRAINT `experiments_ibfk_1` FOREIGN KEY (`userid_creator`) REFERENCES `users` (`userid`),
+  ADD CONSTRAINT `experiments_ibfk_2` FOREIGN KEY (`userid_closer`) REFERENCES `users` (`userid`),
+  ADD CONSTRAINT `experiments_ibfk_3` FOREIGN KEY (`userid_witness`) REFERENCES `users` (`userid`),
+  ADD CONSTRAINT `experiments_ibfk_4` FOREIGN KEY (`rev_id`) REFERENCES `revisions` (`rev_id`);
+
+ALTER TABLE `experiments_tags`
+  ADD CONSTRAINT `userid` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`);
+
+ALTER TABLE `experiments_templates`
+  ADD CONSTRAINT `experiments_templates_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`);
+
+ALTER TABLE `items`
+  ADD CONSTRAINT `items_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`),
+  ADD CONSTRAINT `items_ibfk_2` FOREIGN KEY (`type`) REFERENCES `items_types` (`id`);
+
+ALTER TABLE `items_tags`
+  ADD CONSTRAINT `items_tags_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `items_tags_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `items_tags_ibfk_3` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE `revisions`
+  ADD CONSTRAINT `experiment_id` FOREIGN KEY (`experiment_id`) REFERENCES `experiments` (`id`),
+  ADD CONSTRAINT `revisions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`userid`),
+  ADD CONSTRAINT `revisions_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`),
+  ADD CONSTRAINT `revisions_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `users` (`userid`),
+  ADD CONSTRAINT `revisions_ibfk_4` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`);
+
+ALTER TABLE `uploads`
+  ADD CONSTRAINT `uploads_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`),
+  ADD CONSTRAINT `uploads_ibfk_2` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`),
+  ADD CONSTRAINT `uploads_ibfk_3` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`);
 
 -- ELABFTW
 INSERT INTO `items_types` (`id`, `name`, `bgcolor`, `template`, `tags`) VALUES
