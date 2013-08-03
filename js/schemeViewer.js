bb2230298742f47207fe8bb2bdfa907da6575409
Martin Peeks <martinp23@googlemail.com>
2013-08-03 20:58:48 +0100

    schemeViewer.js: add moleculeViewer function and actually use width/height parameters.

--- js/schemeViewer.js
+++ js/schemeViewer.js
@@ -1,7 +1,12 @@
 function schemeViewer(rxn, height, width) {
+    if(!height) {
+        height = 200;
+    }
+    if(!width) {
+        width = 600;
+    }
     var viewerName = 'viewer' + Math.round(Math.random() * 10000);
-    this[viewerName] = new ChemDoodle.ViewerCanvas(viewerName, 600, 200);
-    debugger;
+    this[viewerName] = new ChemDoodle.ViewerCanvas(viewerName, width, height);
     this[viewerName].specs.atoms_displayTerminalCarbonLabels_2D = true;
     // sets atom labels to be colored by JMol colors, which are easy to recognize
     this[viewerName].specs.atoms_useJMOLColors = true;
@@ -17,5 +22,29 @@ function schemeViewer(rxn, height, width) {
         var reaction_cd = ChemDoodle.readRXN(rxn);
         this[viewerName].loadContent(reaction_cd.molecules, reaction_cd.shapes);
     }
+}
 
+function moleculeViewer(molecule, height, width) {
+    if(!height) {
+        height = 200;
+    }
+    if(!width) {
+        width = 200;
+    }
+    var viewerName = 'viewer' + Math.round(Math.random() * 10000);
+    this[viewerName] = new ChemDoodle.ViewerCanvas(viewerName, width, height);
+    this[viewerName].specs.atoms_displayTerminalCarbonLabels_2D = true;
+    // sets atom labels to be colored by JMol colors, which are easy to recognize
+    this[viewerName].specs.atoms_useJMOLColors = true;
+    // enables overlap clear widths, so that some depth is introduced to overlapping bonds
+    this[viewerName].specs.bonds_clearOverlaps_2D = true;
+    // sets the shape color to improve contrast when drawing figures
+    this[viewerName].specs.shapes_color = 'c10000';
+    
+    this[viewerName].emptyMessage = "No molecule defined";
+
+       debugger;
+    if(molecule) {
+        this[viewerName].loadMolecule(ChemDoodle.readMOL(molecule));
+    }
 }
